b3ca4775856c64a4323164709480d9f1b048b8db
Satya <satya.iosdev@gmail.com>
2016-12-29 13:53:17 -0500

    Used methods like random and responded easily to user input numbers

--- Guess The Number/ViewController.swift
+++ Guess The Number/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    private var randomNumber = 0;
+    private var userNumber = 0;
+    private var countGuesses = 0;
+    private var guessAgain = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomNumber = Int(arc4random_uniform(100));
+
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var guessNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func submitButtonPressed(_ sender: AnyObject) {
+        
+        
+        
+        if guessAgain {
+            guessNumber.text = "";
+            countGuesses = 0;
+            guessAgain = false;
+            
+        }
+        if Int(guessNumber.text!) != nil{
+            let num = Int(guessNumber.text!);
+            countGuesses += 1;
+            
+            if num == randomNumber {
+                
+                resultLabel.text = "Your Guess is Correct with \(countGuesses) Guesses";
+                guessAgain = true;
+            }
+            else if num! < randomNumber {
+                
+                resultLabel.text = "Your Guess is lessthan the number";
+            }
+            
+            else if num! > randomNumber {
+                
+                resultLabel.text = "Your Guess is greaterthan the number";
+            }
+//             randomNumber = Int(arc4random_uniform(100));
+            guessNumber.text = "";
 
+        }
+        else
+        {
+            resultLabel.text = "Please enter valid number";
+//            guessNumber.text = "";
+        }
+        
+        
+        
+        
+        
+    }
+
+    
 
 }
 
